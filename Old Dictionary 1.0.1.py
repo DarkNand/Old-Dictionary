c561ee80519d563fe26305ba1801441647c8235c
DarkNand <netanel.margalit@gmail.com>
2015-08-07 19:34:12 +0300

    Update and rename Old Dictionary 1.py to Old Dictionary 1.0.1.py

--- Old Dictionary 1.0.1.py
+++ Old Dictionary 1.0.1.py
@@ -1,9 +1,9 @@
 from gi.repository import Gtk
 import sqlite3 as sql
 
-class ParseDatabase():
-	def __init__(self):
-		self.db = sql.connect('dictionary.db')
+class ParseGutenberg():
+	def __init__(self, insert):
+		self.db = sql.connect(insert)
 		self.cursor = self.db.cursor()
 	
 	def parse_database(self, topic):
@@ -68,52 +68,99 @@ class ParseDatabase():
 				self.definition =  row[0]
 				return self.definition
 
+class DictionaryObject(Gtk.Grid):
+	def __init__(self, database):
+		Gtk.Grid.__init__(self)
+		self.database = database
+		self.scroller = Gtk.ScrolledWindow()
+		self.scroller.set_size_request(480, 480)
+		self.textView = Gtk.TextView()
+		self.textView.set_editable(False)
+		self.textView.set_cursor_visible(False)
+		self.textView.set_wrap_mode(Gtk.WrapMode(3))
+		self.queryLabel = Gtk.Label("Enter a Topic and Press Go")
+		self.queryLabel.set_justify(Gtk.Justification.CENTER)
+		self.queryEntry = Gtk.Entry()
+		self.goButton = Gtk.Button(label = 'Go')
+		
+		self.scroller.add(self.textView)
+		self.attach(self.queryLabel, 0, 0, 5, 1)
+		self.attach(self.queryEntry,0, 1, 3, 1)
+		self.attach(self.goButton, 3, 1, 2, 1)
+		self.attach(self.scroller, 0, 4, 3, 1)
+
+		self.goButton.connect("clicked", self.goButton_callback)
+		self.queryEntry.connect("activate", self.goButton_callback)
+	
+		
+	def goButton_callback(self, widget):
+		self.query = self.queryEntry.get_text().upper()
+		buff = self.textView.get_buffer()
+		try:
+			definition = self.database.parse_database(self.query)
+			buff.set_text(definition)
+		except IndexError:
+			pass
+			
 class DictionaryWindow(Gtk.Window):
 	def __init__(self):	
 		#This basically ascosiates the init method with a window
 		Gtk.Window.__init__(self, title= "Old Dictionary")
 		
-		self.database = ParseDatabase()
-		
-		# Create a grid and add it to the window
-		grid = Gtk.Grid()
-		self.add(grid) #this works because self points to the window
 		self.connect("delete-event", Gtk.main_quit)
 		
-		#This is how you create children
-		queryLabel = Gtk.Label("Enter a Topic and Press Go")
-		queryLabel.set_justify(Gtk.Justification.CENTER)
-		self.queryEntry = Gtk.Entry()
-		self.goButton = Gtk.Button(label = 'Go')
-		self.textView = Gtk.TextView()
-		self.textView.set_editable(False)
-		self.textView.set_cursor_visible(False)
-		self.textView.set_wrap_mode(Gtk.WrapMode(3))
-		self.scroller = Gtk.ScrolledWindow()
-		self.scroller.set_size_request(480, 480) # FORCE BIGGER SIZE FOR TEXTVIEW
-		self.scroller.add(self.textView)
-		#This is how you link the buttons with methods
-		self.goButton.connect("clicked", self.goButton_callback)
-		self.queryEntry.connect("activate", self.goButton_callback)
+		# Prevent window from resizing
+		self.set_resizable(False)
+		# Load databases
+		self.database = ParseGutenberg('dictionary.db')
+
+		# Header Buttons
+		self.quitW = Gtk.Button(label = "Quit")
+		self.nextPage = Gtk.Button(label = "Next Page")
+		self.prevPage = Gtk.Button(label = "Previous Page")
 		
-		# grid.attach accepts a widget, column, row, width, height
-		grid.attach(queryLabel, 0, 0, 5, 1)
-		grid.attach(self.queryEntry, 0, 1, 3, 1)
-		grid.attach(self.goButton, 3, 1, 1, 1)
-		grid.attach(self.scroller, 0, 4, 4, 1)
+		# Vbox, notebook, headerbar initialization
+		self.vBox = Gtk.VBox()
+		self.headerBar = Gtk.HeaderBar()
+		self.notebook = Gtk.Notebook()
+		self.vBox.pack_start(self.headerBar, True, True, 0)
+		self.vBox.pack_start(self.notebook, True, True, 0)
 		
-	#The following are callback functions for the buttons
-	def goButton_callback(self, widget):
-		query = self.queryEntry.get_text().upper()
-		buff = self.textView.get_buffer()
-		definition = self.database.parse_database(query)
-		try:
-			buff.set_text(definition)
-		except TypeError:
-			pass
+		# Headerbar buttons
+		self.headerBar.pack_start(self.quitW)
+		self.headerBar.pack_end(self.nextPage)
+		self.headerBar.pack_end(self.prevPage)
+		# Add window to vBox
+		self.add(self.vBox) #this works because self points to the window
 		
+		# Connect headerButtons
+		self.quitW.connect("clicked", self.quit_window)
+		self.nextPage.connect("clicked", self.next_page)
+		self.prevPage.connect("clicked", self.prev_page)
+		# Create pages
+		pageOne = DictionaryObject(self.database)
+		pageTwo = DictionaryObject(self.database)
+		pageThree = DictionaryObject(self.database)
+		pageFour = DictionaryObject(self.database)
+		
+		# Add pages
+		self.notebook.append_page(pageOne)
+		self.notebook.append_page(pageTwo)
+		self.notebook.append_page(pageThree)
+		self.notebook.append_page(pageFour)
+		
+	def quit_window(self, widget):
+		Gtk.main_quit()
+		
+	def next_page(self, widget):
+		self.notebook.next_page()
+	
+	def prev_page(self, widget):
+		self.notebook.prev_page()
+
+
 		
 win = DictionaryWindow()
+
 win.show_all()
 Gtk.main()
-				
