fcc24d11ecf681037ccba47da0a9f48b4fdcfe71
DarkNand <netanel.margalit@gmail.com>
2015-08-12 03:53:00 +0300

    Old-Dictionary 1.0.2.py

--- Old Dictionary 1.0.1.py
+++ Old Dictionary 1.0.1.py
@@ -1,6 +1,6 @@
 from gi.repository import Gtk
 import sqlite3 as sql
-
+from random import randint
 class ParseGutenberg():
 	def __init__(self, insert):
 		self.db = sql.connect(insert)
@@ -69,11 +69,12 @@ class ParseGutenberg():
 				return self.definition
 
 class DictionaryObject(Gtk.Grid):
-	def __init__(self, database):
+	def __init__(self, database, notebook):
 		Gtk.Grid.__init__(self)
 		self.database = database
+		self.notebook = notebook
 		self.scroller = Gtk.ScrolledWindow()
-		self.scroller.set_size_request(480, 480)
+		self.scroller.set_size_request(480, 400)
 		self.textView = Gtk.TextView()
 		self.textView.set_editable(False)
 		self.textView.set_cursor_visible(False)
@@ -85,8 +86,8 @@ class DictionaryObject(Gtk.Grid):
 		
 		self.scroller.add(self.textView)
 		self.attach(self.queryLabel, 0, 0, 5, 1)
-		self.attach(self.queryEntry,0, 1, 3, 1)
-		self.attach(self.goButton, 3, 1, 2, 1)
+		self.attach(self.queryEntry,0, 1, 2, 1)
+		self.attach(self.goButton, 2, 1, 2, 1)
 		self.attach(self.scroller, 0, 4, 3, 1)
 
 		self.goButton.connect("clicked", self.goButton_callback)
@@ -99,68 +100,69 @@ class DictionaryObject(Gtk.Grid):
 		try:
 			definition = self.database.parse_database(self.query)
 			buff.set_text(definition)
+			
+			pageNum = self.notebook.get_current_page()
+			identifier = self.notebook.get_nth_page(pageNum)
+			self.notebook.set_tab_label(identifier, Gtk.Label(self.queryEntry.get_text()))
+			
 		except IndexError:
 			pass
 			
 class DictionaryWindow(Gtk.Window):
 	def __init__(self):	
-		#This basically ascosiates the init method with a window
+		#This basically accosiates the init method with a window
 		Gtk.Window.__init__(self, title= "Old Dictionary")
-		
 		self.connect("delete-event", Gtk.main_quit)
 		
 		# Prevent window from resizing
 		self.set_resizable(False)
+		
 		# Load databases
 		self.database = ParseGutenberg('dictionary.db')
 
-		# Header Buttons
+		# Buttons
 		self.quitW = Gtk.Button(label = "Quit")
 		self.nextPage = Gtk.Button(label = "Next Page")
 		self.prevPage = Gtk.Button(label = "Previous Page")
+		self.addTab = Gtk.Button (label = "Add Tab")
+		self.removeTab =  Gtk.Button (label = "Remove Tab")
 		
 		# Vbox, notebook, headerbar initialization
 		self.vBox = Gtk.VBox()
-		self.headerBar = Gtk.HeaderBar()
+		self.hBox = Gtk.HBox()
 		self.notebook = Gtk.Notebook()
-		self.vBox.pack_start(self.headerBar, True, True, 0)
+		self.notebook.set_scrollable(True)
+		self.notebook.popup_enable()
+		
+		#self.vBox.pack_start(self.headerBar, True, True, 0)
+		self.vBox.pack_start(self.hBox, True, True, 0)
+		self.hBox.pack_start(self.addTab, True, True, 0)
+		self.hBox.pack_start(self.removeTab, True, True, 0)
 		self.vBox.pack_start(self.notebook, True, True, 0)
 		
-		# Headerbar buttons
-		self.headerBar.pack_start(self.quitW)
-		self.headerBar.pack_end(self.nextPage)
-		self.headerBar.pack_end(self.prevPage)
 		# Add window to vBox
 		self.add(self.vBox) #this works because self points to the window
 		
-		# Connect headerButtons
-		self.quitW.connect("clicked", self.quit_window)
-		self.nextPage.connect("clicked", self.next_page)
-		self.prevPage.connect("clicked", self.prev_page)
-		# Create pages
-		pageOne = DictionaryObject(self.database)
-		pageTwo = DictionaryObject(self.database)
-		pageThree = DictionaryObject(self.database)
-		pageFour = DictionaryObject(self.database)
+		# Connect Buttons
+		self.addTab.connect("clicked", self.add_tab)
+		self.removeTab.connect("clicked", self.remove_tab)
 		
 		# Add pages
-		self.notebook.append_page(pageOne)
-		self.notebook.append_page(pageTwo)
-		self.notebook.append_page(pageThree)
-		self.notebook.append_page(pageFour)
+		self.notebook.append_page(DictionaryObject(self.database, self.notebook), Gtk.Label("Tab1"))
+		
 		
 	def quit_window(self, widget):
 		Gtk.main_quit()
 		
-	def next_page(self, widget):
-		self.notebook.next_page()
+	def add_tab(self, widget):
+		tempTab = DictionaryObject(self.database, self.notebook)
+		self.notebook.append_page(tempTab, Gtk.Label("Tab" + str(int(self.notebook.get_n_pages()) + 1)))
+		self.notebook.show_all()
 	
-	def prev_page(self, widget):
-		self.notebook.prev_page()
+	def remove_tab(self, widget):
+		currentPage = self.notebook.get_current_page()
+		self.notebook.remove_page(currentPage)
 
-
-		
 win = DictionaryWindow()
-
 win.show_all()
 Gtk.main()
